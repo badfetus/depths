fb4a124743c91a046a35e5770e6bb932bee4b60c
unknown <berkezorlu@gmail.com>
2025-04-06 23:57:21 +0200

    level complete, functionality almost complete

--- player.gd
+++ player.gd
@@ -17,6 +17,28 @@ func _physics_process(delta: float) -> void:
 	elif(holdDuration > 0):
 		jump()
 		holdDuration = 0
+	if(position.y < -5):
+		position.y = 3
+		position.x = 0
+		position.z = 0
+		apply_central_impulse(linear_velocity * -1)
+		
+	handleCheats()
+	
+func handleCheats():
+	if Input.is_action_just_pressed("Cheat"): 
+			var camRot = camera.rotation
+			var horizontalRotation = camRot.y
+	
+			var impulse = Vector3()
+			impulse.y = getNormalisedAngle(camRot.x)
+			impulse.y = impulse.y * 30 * 1
+			impulse.x = sin(horizontalRotation) * 30 * 1 * abs(cos(getNormalisedAngle(camRot.x)))
+			impulse.z = cos(horizontalRotation) * 30 * 1 * abs(cos(getNormalisedAngle(camRot.x)))
+			apply_central_impulse(impulse)
+	if Input.is_action_just_pressed("Cheat2"): 
+		apply_central_impulse(linear_velocity * -1)
+
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.is_pressed() and on_floor:
@@ -25,28 +47,25 @@ func _input(event: InputEvent) -> void:
 		jumping = false
 
 var lastJump = -100
+var leeway = 0.1
 
 func jump():
-	if(currTime - lastJump < 0.05):
+	if(currTime - lastJump < leeway):
 		return
 	
 	lastJump = currTime
-	print("jumping")
 	
 	var camRot = camera.rotation
 	var horizontalRotation = camRot.y
-
-	print("horRot: " + str(horizontalRotation))
 	
 	holdDuration = min(0.5, holdDuration)
 	holdDuration = max(0.1, holdDuration)
 	var impulse = Vector3()
 	impulse.y = getNormalisedAngle(camRot.x)
 	impulse.y = max(0.3, impulse.y) * 30 * holdDuration
-	impulse.x = -sin(horizontalRotation) * 30 * holdDuration * abs(cos(getNormalisedAngle(camRot.x)))
-	impulse.z = -cos(horizontalRotation) * 30 * holdDuration * abs(cos(getNormalisedAngle(camRot.x)))
+	impulse.x = sin(horizontalRotation) * 30 * holdDuration * abs(cos(getNormalisedAngle(camRot.x)))
+	impulse.z = cos(horizontalRotation) * 30 * holdDuration * abs(cos(getNormalisedAngle(camRot.x)))
 	apply_central_impulse(impulse)
-	print(impulse)
 
 func getNormalisedAngle(angle: float) -> float:
 	var normAngle = fmod(angle, (PI * 2))
@@ -59,15 +78,17 @@ var currTime = 0
 var lastTimeOnFloor = -100
 
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
+	on_floor = false
+	
 	var i := 0
 	while i < state.get_contact_count():
 		var normal := state.get_contact_local_normal(i)
-		var this_contact_on_floor = normal.dot(Vector3.UP) > 0.99
+		var this_contact_on_floor = normal.dot(Vector3.UP) > 0.5
 
 		# boolean math, will stay true if any one contact is on floor
 		on_floor = on_floor or this_contact_on_floor
 		i += 1
 	if (on_floor):
 		lastTimeOnFloor = currTime
-	elif (currTime - lastTimeOnFloor < 0.05):
+	elif (currTime - lastTimeOnFloor < leeway):
 		on_floor = true
