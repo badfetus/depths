73c7ba3a095cc598834b854d5262a05580fc4e1e
unknown <berkezorlu@gmail.com>
2025-04-07 04:39:36 +0200

    fixed critical bug - quick restart did not work when built (only worked in editor)

--- world.gd
+++ world.gd
@@ -2,7 +2,7 @@ extends Node3D
 
 func _physics_process(_delta: float) -> void:
 	if Input.is_action_just_pressed("Restart"): 
-		var scene = load("res://" + self.name + ".tscn")
+		var scene = load("res://world.tscn")
 		get_tree().change_scene_to_packed(scene)
 
 func _input(event: InputEvent) -> void:
